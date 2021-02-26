ae5b71c95f838ca484c63c4d358fcdd31fb88803
Dheerajk1994 <dheerajk.har@gmail.com>
2021-02-25 21:38:25 -0800

    Fixed issue with release date not showing

--- app/src/main/java/com/example/artistfinder/model/result/ApiResult.kt
+++ app/src/main/java/com/example/artistfinder/model/result/ApiResult.kt
@@ -5,7 +5,7 @@ data class ApiResult(val results : MutableList<TrackPoko>)
 data class TrackPoko(
         var artistName : String,
         var trackName : String,
-        var releaseData : String,
+        var releaseDate : String,
         var trackPrice : String,
         var primaryGenreName : String
 )
--- app/src/main/java/com/example/artistfinder/ui/TrackDataViewHolder.kt
+++ app/src/main/java/com/example/artistfinder/ui/TrackDataViewHolder.kt
@@ -15,7 +15,7 @@ class TrackDataViewHolder(itemView: View) : DataViewHolder(itemView){
     override fun bindTrack(apiResult: TrackPoko) {
         itemView.tv_artist_name.text = apiResult.artistName
         itemView.tv_track_name.text = apiResult.trackName
-        itemView.tv_release_date.text = apiResult.releaseData
+        itemView.tv_release_date.text = apiResult.releaseDate
         itemView.tv_primary_genre_name.text = apiResult.primaryGenreName
         itemView.tv_track_price.text = "$ ${apiResult.trackPrice}"
     }
